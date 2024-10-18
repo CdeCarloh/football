89887caabfb1c54b6d7a0bfbafdc2bb8bf797596
CdeCarloh <carlosperezsantana04@gmail.com>
2024-10-18 16:50:55 +0100

    hola

--- js/bye.js
+++ js/bye.js
@@ -1,5 +1,11 @@
-function hideHim() {
-    let secondCr7 = document.getElementById("second-cr7");
-    secondCr7.style.visibility = "hidden";
-
+function listenToUser() {
+  let secondCr7 = document.getElementById("second-cr7");
+  secondCr7.addEventListener("click", hideSecondCr7);
 }
+
+function hideSecondCr7() {
+  let secondCr7 = document.getElementById("second-cr7");
+  secondCr7.style.visibility = "hidden";
+}
+
+listenToUser();
